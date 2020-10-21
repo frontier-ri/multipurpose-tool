7da1480284fdf55f6910968e535310383ef42e46
Takahiro55555 <tkhr.corei7@gmail.com>
2020-10-21 17:06:33 +0900

    add: csvファイルからxmlファイルへ変換し、zipでダウンロードできるようにした。 要リファクタリング

--- js/csv-to-snopy-xml.js
+++ js/csv-to-snopy-xml.js
@@ -4,22 +4,151 @@ document.getElementById('inputfile').oninput = (e) => {
     var file = e.target.files;
     var reader = new FileReader();
     reader.readAsText(file[0]);
-    console.log(file);
+    // console.log(file);
+    document.getElementById('file-text').innerText = "";
+    const zip = new Zlib.Zip();
     reader.onload = function () {
-        document.getElementById('file-text').innerText = reader.result;
-        console.log(CSVToArray(reader.result));
-        createCsv(reader.result, "new-" + file[0].name);
+        const csvArray = CSVToArray(reader.result);
+        for (let i = 1; i < csvArray.length; i++) {
+            const data = {};
+            for (let j = 0; j < csvArray[0].length; j++) {
+                const key = csvArray[0][j];
+                data[key] = csvArray[i][j];
+            }
+            if (data["snoopyID"] === "") {
+                console.log("Invalid snoopyID (counter: " + String(i) + ")");
+                break;
+            }
+            const xmlString = createSnoXmlString(data);
+            if (xmlString === null) {
+                break;
+            }
+            zip.addFile(new TextEncoder().encode(xmlString), {
+                filename: new TextEncoder().encode(data["snoopyID"] + ".xml")
+            });
+        }
+        const compressData = zip.compress();
+        const blob = new Blob([compressData], { "type": "application/zip" });
+        window.URL = window.URL || window.webkitURL;
+
+        const link = document.getElementById('donwload');
+        link.setAttribute("href", window.URL.createObjectURL(blob));
+        link.setAttribute("download", "convertedCsv.zip");
     };
 }
 
 /**** ファイルの生成 ****/
-function createCsv(content, name) {
+function createCsvFile(content, name) {
     const link = document.getElementById('donwload');
     const blob = new Blob([content], { "type": "text/csv" });
     window.URL = window.URL || window.webkitURL;
 
     link.setAttribute("href", window.URL.createObjectURL(blob));
     link.setAttribute("download", name);
+}
+
+function createSnoXmlString(data) {
+    /**
+     * data の中身は、列名をキーにした辞書型であること、CSVファイル１行分のデータがあることを期待する
+     */
+
+    // data に含まれていると期待するキー
+    const expectedKeys = [
+        "snoopyID",
+        "dataquality",
+        "openstatus",
+        "lastupdate",
+        "blindnote",
+        "organism",
+        "gene_name",
+        "family_name",
+        "alias",
+        "family_base",
+        "chrom_or_contig",
+        "genome_position",
+        "strand",
+        "box:type='c'",
+        "box:type='d'",
+        "box:type='h'",
+        "box:type='aca'",
+        "host_id",
+        "host_gene",
+        "host_intron",
+        "host_position",
+        "organization",
+        "mod_type",
+        "mod_rna",
+        "mod_site",
+        "duplex_region",
+        "accession",
+        "evidence",
+        "note",
+        "sequence"
+    ];
+
+    /**** data のバリデーション ****/
+    const lostValues = [];
+    for (let i = 0; i < expectedKeys.length; i++) {
+        const key = expectedKeys[i];
+        if (data[key] === undefined) {
+            lostValues.push(key);
+        }
+    }
+    if (0 < lostValues.length) {
+        console.log("On function 'createSnoXml', expected below data\n" + JSON.stringify(lostValues));
+        return null;
+    }
+
+    /**** xml 生成 ****/
+    const xmlDoc = document.implementation.createDocument(null, "snopy");
+    const elements = xmlDoc.getElementsByTagName("snopy");
+    for (let i = 0; i < expectedKeys.length; i++) {
+        const key = expectedKeys[i];
+        let node;
+        switch (key) {
+            case "snoopyID":
+                elements[0].setAttribute("id", data[key]);
+                break;
+
+            case "box:type='c'":
+                node = xmlDoc.createElement("box");
+                node.textContent = data[key];
+                node.setAttribute("type", "c");
+                elements[0].appendChild(node);
+                break;
+
+            case "box:type='d'":
+                node = xmlDoc.createElement("box");
+                node.textContent = data[key];
+                node.setAttribute("type", "d");
+                elements[0].appendChild(node);
+                break;
+
+            case "box:type='h'":
+                node = xmlDoc.createElement("box");
+                node.textContent = data[key];
+                node.setAttribute("type", "h");
+                elements[0].appendChild(node);
+                break;
+
+            case "box:type='aca'":
+                node = xmlDoc.createElement("box");
+                node.textContent = data[key];
+                node.setAttribute("type", "aca");
+                elements[0].appendChild(node);
+                break;
+
+            default:
+                node = xmlDoc.createElement(key);
+                node.textContent = data[key];
+                elements[0].appendChild(node);
+                break;
+        }
+    }
+
+    const xmlSerializer = new XMLSerializer();
+    const xmlString = xmlSerializer.serializeToString(xmlDoc);
+    return xmlString;
 }
 
 /**** CSV Parser ****/
